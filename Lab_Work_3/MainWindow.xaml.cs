3e80ee7aa0b47126cc15c55c6ef459aa60e2b9d7
ArtyomKolosov2 <62713674+ArtyomKolosov2@users.noreply.github.com>
2020-09-07 23:51:47 +0300

    LabThree Ready

--- Lab_Work_3/MainWindow.xaml.cs
+++ Lab_Work_3/MainWindow.xaml.cs
@@ -30,7 +30,6 @@ namespace Lab_Work_3
             string text_x1 = GetX1_Input.Text;
             string text_x2 = GetX2_Input.Text;
             string text_n = GetN_Input.Text;
-            string text_h = GetH_Input.Text;
             bool Is_Succes = true;
             if (!double.TryParse(text_x1, out double x1))
             {
@@ -42,12 +41,7 @@ namespace Lab_Work_3
                 Is_Succes = false;
                 text_x2 = "Invalid data!";
             }
-            if (!double.TryParse(text_n, out double n))
-            {
-                Is_Succes = false;
-                text_n = "Invalid data!";
-            }
-            if (!double.TryParse(text_h, out double h))
+            if (!int.TryParse(text_n, out int n))
             {
                 Is_Succes = false;
                 text_n = "Invalid data!";
@@ -57,16 +51,57 @@ namespace Lab_Work_3
                "Лаб. раб. №1 Ст.Гр. 10701219 Колосов А.А\n" +
                $"x1 = {text_x1}\n" +
                $"x2 = {text_x2}\n" +
-               $"N = {text_n}\n" +
-               $"H = {text_h}\n";
+               $"N = {text_n}\n";
             if (Is_Succes)
             {
-
+                StartCount(x1, x2, n);
             }
         }
-        private void StartCount(double x1, double x2, double n, double h)
+        private void StartCount(double xn, double xk,  int n)
         {
+            double h = (xk - xn) / n;
+            StringBuilder result_str = new StringBuilder();
+            for (int i = 0; i < n; i++, xn+=h)
+            {
+                xn = Math.Round(xn, 4);
+                result_str.Append
+                (
+                    $"{(i+1).ToString()}. xn = {xn}, S(x) = {Math.Round(GetSXFunctionResult(xn), 4)} \t " +
+                    $"Y(x) = {Math.Round(GetYXFunctionResult(xn), 4)}\n"
+                );
+            }
+            ResultTextBox.Text += result_str.ToString();
+        }
 
+        private double GetSXFunctionResult(double x)
+        {
+            double Co = 1;
+            double f = Co;
+            double sum = f;
+            int k = 0;
+            while (k < 500)
+            {
+                double T = -(Math.Pow(x, 2) * (k + 1)) / ((2 * Math.Pow(k, 2) + 1) * (2 * k + 1));
+                f *= T;
+                sum += f;
+                k++;
+            }
+            return sum;
+        }
+
+        private double GetYXFunctionResult(double x)
+        {
+            return (1 - ((x * x) / 2d)) * Math.Cos(x) - (x / 2d) * Math.Sin(x);
+        }
+
+        private int Factorial(int number)
+        {
+            int result = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                result *= i;
+            }
+            return result;
         }
     }
 }
