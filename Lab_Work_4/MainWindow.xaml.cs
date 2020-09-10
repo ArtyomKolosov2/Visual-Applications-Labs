77b42048372cbe976dfffa912aef80ca86a21c88
ArtyomKolosov2 <62713674+ArtyomKolosov2@users.noreply.github.com>
2020-09-10 16:49:49 +0300

    TextBox added, arraylogic added

--- Lab_Work_4/MainWindow.xaml.cs
+++ Lab_Work_4/MainWindow.xaml.cs
@@ -30,12 +30,23 @@ namespace Lab_Work_4
 
         private void MainDataGridView_CellEndEdit(object sender, DataGridViewCellEventArgs e)
         {
-
+            DataGridView dataGrid = (DataGridView)sender;
+            if (_mainDoubleArray != null)
+            {
+                int column = e.ColumnIndex, row = e.RowIndex;
+                string data = dataGrid[column, row]?.Value?.ToString();
+                if (!int.TryParse(data, out _mainDoubleArray[column, row]))
+                {
+                    data = string.Empty;
+                }
+                dataGrid[column, row].Value = data;
+                dataGrid.Refresh();
+            }
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            
+            MainDataGridView.AllowUserToAddRows = false;
         }
 
         private void ResizeButton_Click(object sender, RoutedEventArgs e)
@@ -47,25 +58,30 @@ namespace Lab_Work_4
         {
             string text_m = SizeTextBoxM.Text;
             string text_n = SizeTextBoxN.Text;
-            if (!int.TryParse(text_m, out int m))
+            if (!int.TryParse(text_m, out int m) || !int.TryParse(text_n, out int n))
             {
                 return;
             }
-            if (!int.TryParse(text_n, out int n))
+            if ((m + n) > ushort.MaxValue || m > ushort.MaxValue || n > ushort.MaxValue)
             {
+                SizeTextBoxM.Text = "Error: to much!";
+                SizeTextBoxN.Text = "Error: to much!";
                 return;
             }
-            _mainDoubleArray = new int[m, n];
+            MainDataGridView.Rows.Clear();
             MainDataGridView.RowCount = m;
-            MainDataGridView.ColumnCount = n;
+            MainDataGridView.ColumnCount = n;    
+            _mainDoubleArray = new int[m, n];
             for (int i = 0; i < n; i++)
             {
-                MainDataGridView.Columns[i].HeaderText = $"i = {i.ToString()}";
+                MainDataGridView.Columns[i].HeaderText = $"i={i.ToString()}";
             }
             for (int i = 0; i < m; i++)
             {
-                MainDataGridView.Rows[i].HeaderCell.Value = $"j = {i.ToString()}";
+                MainDataGridView.Rows[i].HeaderCell.Value = $"j={i.ToString()}";
             }
+            MainDataGridView.AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders);
+            GC.Collect();
         }
     }
 }
