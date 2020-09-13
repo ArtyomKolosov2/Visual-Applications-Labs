1a5f76ab9dc6ec19c4d61b9f6340a394cd19c7b7
ArtyomKolosov2 <62713674+ArtyomKolosov2@users.noreply.github.com>
2020-09-13 17:20:03 +0300

    Lab5

---
+++ Lab_Work_5/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_Work_5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<string> _strings = new ObservableCollection<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void SaveCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            string text = StringComboBox.Text;
+            StringComboBox.Text = string.Empty;
+            
+        }
+    }
+    
+}
---
+++ Lab_Work_5/modules/AddTextCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Lab_Work_5.modules
+{
+	public static class CustomCommands
+	{
+		public static readonly RoutedUICommand Save = new RoutedUICommand
+			(
+				"Save",
+				"SaveFromComboBox",
+				typeof(CustomCommands),
+				new InputGestureCollection()
+				{
+					new KeyGesture(Key.Return)
+				}
+			);
+	}
+}
