af3b93a5127b6d69da0758ff7e61b604639fc350
artyomkolosov2 <artyomkolosov2@gmail.com>
2020-10-15 00:29:19 +0300

    test

--- Lab_Work_8_Framework/MainWindow.xaml.cs
+++ Lab_Work_8_Framework/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Math;
 
 namespace Lab_Work_8_Framework
 {
@@ -20,6 +23,10 @@ namespace Lab_Work_8_Framework
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<double> Points = new List<double>();
+        
+        public SeriesCollection SeriesCollection { get; private set; }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,13 +34,54 @@ namespace Lab_Work_8_Framework
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            var c = new LiveChartVM();
-            MyChart.DataContext = c;
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Values = new ChartValues<double> { 3, 5, 7, 4 }
+                },
+            };
+            MyChart.DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            MyChart.Series = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Values = new ChartValues<double> {1, 1, 1, 4 }
+                },
+            };
+            /*double x1 = Convert.ToDouble(TextBox_GetX1.Text);
+            double x2 = Convert.ToDouble(TextBox_GetX2.Text);
+            double h = (x2 - x1) / 100;
+            MyChart.DataContext = LiveChartVM.GetPoints(x1, x2, h);*/
         }
     }
-    public class LiveChartVM
+    public class P
     {
-        public double[] SPoints = new double[] { 1, 2, 3, 4 };
-        public double[] CPoints = new double[] { 1, 2, 3, 4 };
+        public List<double> Points { get; set; }
+    }
+    public static class LiveChartVM
+    {
+        public static P GetPoints(double x1, double x2, double h)
+        {
+            var result = new List<double>();
+            for (;x1 <= x2; x1 += h)
+            {
+                result.Add(CalculateOperation(x1, 1, 1));
+            }
+            return new P { Points = result };
+        }
+
+        private static double CalculateOperation(double x, double y, double z)
+        {
+            return Pow(y, Pow(x, (double)1 / 3)) +
+                Pow(Cos(y), 3) *
+                (Abs(x - y) * (1d + (Pow(Sin(z), 2)) /
+                (Sqrt(x + y))) / (Pow(Math.E, x - y) + (x / 2d)));
+        }
     }
 }
