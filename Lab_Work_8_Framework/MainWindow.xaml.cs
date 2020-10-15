5cf230f99df05dd3f402b671cfc76b83a295a2c6
ArtyomKolosov2 <ArtyomKolosov2@users.noreply.github.com>
2020-10-15 13:59:43 +0300

    sd

--- Lab_Work_8_Framework/MainWindow.xaml.cs
+++ Lab_Work_8_Framework/MainWindow.xaml.cs
@@ -29,7 +29,7 @@ namespace Lab_Work_8_Framework
 
         public MainWindow()
         {
-            InitializeComponent(); 
+            InitializeComponent();  
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
