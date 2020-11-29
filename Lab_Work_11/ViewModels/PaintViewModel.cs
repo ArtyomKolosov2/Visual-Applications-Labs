e01dc11c5dcde73822cec0e5576d93148c6303b0
ArtyomKolosov2 <ArtyomKolosov2@users.noreply.github.com>
2020-11-29 18:26:07 +0300

    Done

---
+++ Lab_Work_11/ViewModels/PaintViewModel.cs
@@ -0,0 +1,124 @@
+﻿using Lab_Work_11.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Work_11.ViewModels
+{
+    public class PaintViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int _width;
+        private int _height;
+        private double _x1;
+        private double _x2;
+        private double _y1;
+        private double _y2;
+        private double _startSector;
+        private double _endSector;
+
+        private int _inkWidth = 1;
+        private bool _isFilled;
+
+        public GeometryEnum Geometry { get; set; } = GeometryEnum.Ellipse;
+
+        public int Width
+        {
+            get => _width;
+            set
+            {
+                _width = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Height
+        {
+            get => _height;
+            set
+            {
+                _height = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double X1
+        {
+            get => _x1;
+            set
+            {
+                _x1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double X2
+        {
+            get => _x2;
+            set
+            {
+                _x2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Y1
+        {
+            get => _y1;
+            set
+            {
+                _y1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Y2
+        {
+            get => _y2;
+            set
+            {
+                _y2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double EndSector
+        {
+            get => _endSector;
+            set
+            {
+                _endSector = Math.Abs(value) % 360;
+                OnPropertyChanged();
+            }
+        }
+
+        public int InkWidth
+        {
+            get => _inkWidth;
+            set
+            {
+                _inkWidth = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsFilled
+        {
+            get => _isFilled;
+            set
+            {
+                _isFilled = value;
+                OnPropertyChanged();
+            }
+        }
+        private void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
--- Lab_Work_11/Views/Window3.xaml.cs
+++ Lab_Work_11/Views/Window3.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Lab_Work_11.Enum;
+using Lab_Work_11.ViewModels;
+using Lab_Work_9.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +22,136 @@ namespace Lab_Work_11.Views
     /// </summary>
     public partial class Window3 : Window
     {
+        public RGBViewModel ColorViewModel { get; set; } = new();
+        public PaintViewModel PaintViewModel { get; set; } = new();
         public Window3()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            GeometryChoiceLabel.Content = "Выбрано: Rectangle";
+            PaintViewModel.Geometry = GeometryEnum.Rectangle;
+            TextBox_InputX2.IsReadOnly = true;
+            TextBox_InputY2.IsReadOnly = true;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            GeometryChoiceLabel.Content = "Выбрано: Line";
+            PaintViewModel.Geometry = GeometryEnum.Line;
+            TextBox_InputX2.IsReadOnly = false;
+            TextBox_InputY2.IsReadOnly = false;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            GeometryChoiceLabel.Content = "Выбрано: Sector";
+            PaintViewModel.Geometry = GeometryEnum.Sector;
+            TextBox_InputX2.IsReadOnly = true;
+            TextBox_InputY2.IsReadOnly = true;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            PaintViewModel.Geometry = GeometryEnum.Ellipse;
+            GeometryChoiceLabel.Content = "Выбрано: Ellipse";
+            TextBox_InputX2.IsReadOnly = true;
+            TextBox_InputY2.IsReadOnly = true;
+        }
+
+        private Shape GetShape()
+        {
+            int height = PaintViewModel.Height;
+            return PaintViewModel.Geometry switch
+            {
+                GeometryEnum.Line => new Line 
+                { 
+                    X1=PaintViewModel.X1,
+                    Y1=PaintViewModel.Y1,
+                    X2=PaintViewModel.X2,
+                    Y2=PaintViewModel.Y2,
+                },
+                GeometryEnum.Ellipse => new Ellipse()
+                {
+
+                    Width = PaintViewModel.Width,
+                    Height = PaintViewModel.Height,
+                    RenderTransform = new TranslateTransform() { X = PaintViewModel.X1, Y = PaintViewModel.Y1 },
+                },
+                GeometryEnum.Sector => new Path 
+                {
+                    Data=Geometry.Parse($"M0,0 L0,-{height} A{height},{height} 0 0 {(PaintViewModel.EndSector > 180 ? 0 : 1)} {height * Math.Sin(PaintViewModel.EndSector * (Math.PI / 180d))}, {-height * Math.Cos(PaintViewModel.EndSector * (Math.PI / 180d))} z"), 
+                    RenderTransform = new TranslateTransform() { X = PaintViewModel.X1, Y = PaintViewModel.Y1 } 
+                },
+                GeometryEnum.Rectangle => new Rectangle
+                {
+                    Width = PaintViewModel.Width,
+                    Height = PaintViewModel.Height,
+                    RenderTransform = new TranslateTransform() { X = PaintViewModel.X1, Y = PaintViewModel.Y1 },
+                },
+                _ => throw new ArgumentException("Geomerty didn't choosed")
+            };
+        }
+        private void PaintItButton_Click(object sender, RoutedEventArgs e)
+        {
+            var brush = new SolidColorBrush(ColorViewModel.CurrentColor);
+            var geometry = GetShape();
+            geometry.Stroke = brush;
+            geometry.StrokeThickness = PaintViewModel.InkWidth;
+            if (PaintViewModel.IsFilled)
+            {
+                geometry.Fill = brush;
+            }
+            MainPaintCanvas.Children.Add
+            (
+                geometry
+            );
+            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataContext = this;
+        }
+
+        private void ChangeColorButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new RGBColorWidget(ColorViewModel);
+            dialog.ShowDialog();
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainPaintCanvas.Children.Clear();
+        }
+
+        private void Border_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            MainPaintCanvas.Width = e.NewSize.Width;
+            MainPaintCanvas.Height = e.NewSize.Height;
+        }
+
+        private void TextPaintButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextInput.Text.Length > 0)
+            {
+                MainPaintCanvas.Children.Add
+                (
+                    new TextBlock
+                    {
+                        Text = TextInput.Text,
+                        RenderTransform = new TranslateTransform() 
+                        { 
+                            X = PaintViewModel.X1, 
+                            Y = PaintViewModel.Y1 
+                        },
+                        FontSize = PaintViewModel.InkWidth,
+                        Foreground = new SolidColorBrush(ColorViewModel.CurrentColor)
+                    }
+                );
+            }
         }
     }
 }
